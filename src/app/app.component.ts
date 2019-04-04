9aa8242395d85ab8728b474cc5e23b6e55cd224f
Sanjana Dodley <sanjanadodley@yahoo.com>
2019-04-04 14:59:22 -0400

    ui changes for upload button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Subscription} from 'rxjs/Subscription';
-import {RewardsApiService} from './rewards/rewards-api.service';
-import {CreditCardList, SpendingList} from './rewards/rewards-api.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { RewardsApiService } from './rewards/rewards-api.service';
+import { CreditCardList, SpendingList } from './rewards/rewards-api.model';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +13,15 @@ export class AppComponent implements OnInit, OnDestroy {
   creditCardsListSub: Subscription;
   creditCardsList: CreditCardList;
   spendingListSub: Subscription;
+  spendingListTableSub: Subscription;
+  milesCardSub: Subscription;
+
+  transactionsSub: Subscription;
+
+  cashbackCard: string;
+  pointsCard: string;
+  milesCard: any;
+  ourFile: File; // hold our file
 
   spendingList: SpendingList;
 
@@ -20,7 +29,7 @@ export class AppComponent implements OnInit, OnDestroy {
   myDataSetLabels: Array<any> = [];
 
 
-  public chartType: string = 'doughnut';
+  public chartType = 'doughnut';
 
 
   public chartDatasets: Array<any> = [
@@ -33,7 +42,7 @@ export class AppComponent implements OnInit, OnDestroy {
   public chartColors: Array<any> = [
     {
       backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'], //'Red', 'Green', 'Yellow', 'Grey', 'Dark Grey'
-      hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774','4D5360'],
+      hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774', '4D5360'],
       borderWidth: 2,
     }
   ];
@@ -49,7 +58,7 @@ export class AppComponent implements OnInit, OnDestroy {
       position: 'left',
       labels: {
         font: 10,
-        boxWidth: 20 
+        boxWidth: 20
       }
     }
   };
@@ -61,36 +70,45 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    //delete table by api
+
+    this.milesCardSub = this.rewardsApi
+      .getOptimizedCashBack()
+      .subscribe(res => {
+        this.milesCard = res;
+        console.log(this.milesCard);
+      });
+
     this.creditCardsListSub = this.rewardsApi
       .getCreditCards()
       .subscribe(res => {
-          this.creditCardsList = res;
-          console.log(this.creditCardsList.cards);
-        },
+        this.creditCardsList = res;
+        console.log(this.creditCardsList.cards);
+      },
         console.error
       );
 
     this.spendingListSub = this.rewardsApi
       .getSpending()
       .subscribe(res => {
-          let temp = this;
-          this.spendingList = res;
-          console.log(this.spendingList.spending);
-          let iter = this.spendingList.spending;
-          for (let entry of iter) {
-            this.myDataSet.push(entry.amount_spent);
-            temp.chartDatasets[0].data.push(entry.amount_spent);
-            this.myDataSetLabels.push(entry.category);
-            temp.chartLabels.push(entry.category);
-            temp.chartColors[0].backgroundColor.push(this.getRandomColor());
-          }
+        let temp = this;
+        this.spendingList = res;
+        console.log(this.spendingList.spending);
+        let iter = this.spendingList.spending;
+        for (let entry of iter) {
+          this.myDataSet.push(entry.amount_spent);
+          temp.chartDatasets[0].data.push(entry.amount_spent);
+          this.myDataSetLabels.push(entry.category);
+          temp.chartLabels.push(entry.category);
+          temp.chartColors[0].backgroundColor.push(this.getRandomColor());
+        }
 
-          console.log(temp.chartDatasets[0].data);
-          console.log(temp.chartColors[0].backgroundColor);
-          // this.chartDatasets[0].data = this.myDataSet;
-          console.log(this.myDataSet);
-          console.log(this.myDataSetLabels);
-        },
+        console.log(temp.chartDatasets[0].data);
+        console.log(temp.chartColors[0].backgroundColor);
+        // this.chartDatasets[0].data = this.myDataSet;
+        console.log(this.myDataSet);
+        console.log(this.myDataSetLabels);
+      },
         console.error
       );
 
@@ -101,31 +119,65 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.creditCardsListSub.unsubscribe();
     this.spendingListSub.unsubscribe();
+    this.milesCardSub.unsubscribe();
   }
 
- getRandomColor(): string {
+  openInput() {
+    // your can use ElementRef for this later
+    document.getElementById('fileInput').click();
+  }
+
+  fileChange(files: File[]) {
+    if (files.length > 0) {
+      this.ourFile = files[0];
+    }
+  }
+
+  upload() {
+    console.log('sending this to server', this.ourFile);
+    this.transactionsSub = this.rewardsApi.postTransactions(this.ourFile).subscribe(res => {
+      this.spendingListTableSub = this.rewardsApi.getSpendingTable().subscribe(result => {
+        console.log(this.myDataSet[0]);
+        this.myDataSet.splice(0, this.myDataSet.length);
+        this.myDataSetLabels.splice(0, this.myDataSetLabels.length);
+        const temp = this;
+        this.spendingList = result;
+        console.log(this.spendingList.spending);
+        const iter = this.spendingList.spending;
+        for (const entry of iter) {
+          this.myDataSet.push(entry.amount_spent);
+          temp.chartDatasets[0].data.push(entry.amount_spent);
+          this.myDataSetLabels.push(entry.category);
+          temp.chartLabels.push(entry.category);
+          temp.chartColors[0].backgroundColor.push(this.getRandomColor());
+        }
+      });
+    });
+  }
+
+  getRandomColor(): string {
     const letters = '0123456789ABCDEF';
     let color = '#';
-    let a =  '#F7464A';
-    let b =  '#46BFBD';
-    let c =  '#FDB45C';
-    let d =  '#949FB1';
-    let e =  '#4D5360';
+    let a = '#F7464A';
+    let b = '#46BFBD';
+    let c = '#FDB45C';
+    let d = '#949FB1';
+    let e = '#4D5360';
 
     let temp = Math.floor(Math.random() * Math.floor(5));
     switch (temp) {
-        case 0:
-          return a;
-        case 1:
-          return b;
-        case 2:
-          return c;
-        case 3:
-          return d;
-        case 4:
-          return e;
-        case 5:
-          return a;
+      case 0:
+        return a;
+      case 1:
+        return b;
+      case 2:
+        return c;
+      case 3:
+        return d;
+      case 4:
+        return e;
+      case 5:
+        return a;
     }
     return color;
   }
--- src/app/rewards/rewards-api.service.ts
+++ src/app/rewards/rewards-api.service.ts
@@ -1,12 +1,13 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
-import {API_URL} from '../env';
-import {CreditCardList} from './rewards-api.model';
+import { API_URL } from '../env';
+import { CreditCardList } from './rewards-api.model';
 
 @Injectable()
 export class RewardsApiService {
+
 
   constructor(private http: HttpClient) {
   }
@@ -24,8 +25,42 @@ export class RewardsApiService {
 
   // GET list of categories, future events
   getSpending(): Observable<any> {
-          return this.http
-        .get(`${API_URL}/spending`)
-        .catch(RewardsApiService._handleError);
+    return this.http
+      .get(`${API_URL}/spending`)
+      .catch(RewardsApiService._handleError);
+  }
+
+  getSpendingTable(): Observable<any> {
+    return this.http
+      .get(`${API_URL}/spending/user`)
+      .catch(RewardsApiService._handleError);
+  }
+
+  getOptimizedCashBack(): Observable<any> {
+    return this.http
+      .get(`${API_URL}/optimized/CASHBACK`)
+      .catch(RewardsApiService._handleError);
+  }
+  getOptimizedPoints(): Observable<any> {
+    return this.http
+      .get(`${API_URL}/optimized/POINTS`)
+      .catch(RewardsApiService._handleError);
+  }
+  getOptimizedMiles(): Observable<any> {
+    return this.http
+      .get(`${API_URL}/optimized/MILES`)
+      .catch(RewardsApiService._handleError);
+  }
+
+  postTransactions(fileVar: File): Observable<any> {
+    const HttpUploadOptions = {
+      headers: new HttpHeaders({ "Accept": "application/json" })
     }
+    let input = new FormData();
+    // Add your values in here
+    input.append('file', fileVar);
+    return this.http
+      .post<any>(`${API_URL}/transactions`, input, HttpUploadOptions)
+      .catch(RewardsApiService._handleError);
+  }
 }
